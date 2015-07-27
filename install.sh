c025efa19e065492dc3cdfa3717f7be1e0087e3e
Zhe Wang <zwang.contact@gmail.com>
2015-07-26 23:08:05 -0700

    add aria2 config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/Projects/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc"    # list of files/folders to symlink in homedir
+files="vimrc zshrc aria2.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/.dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
